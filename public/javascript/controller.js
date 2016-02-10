98c75023fc45311a696285295b7ad6a94612e116
julidon <tomberthaud@gmail.com>
2016-02-11 00:01:38 +0100

    merge corrections

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -7,13 +7,8 @@
 
 var devicecatApp = angular.module('deviceApp', []);
 
-<<<<<<< HEAD
-deviceApp.controller('deviceListCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('http://dev2.red-cloud.io/device/result').success(function(data) {
-=======
 devicecatApp.controller('deviceListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('device/device.json').success(function(data) {
->>>>>>> d5daf215e1fa955a0f6e7be77f9154619342b503
         $scope.deviceData = data;
     });
 }]);
