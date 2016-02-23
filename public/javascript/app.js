ddcf6fbc765607a32fa7f5b3500482bc8f981621
julidon <tomberthaud@gmail.com>
2016-02-23 16:33:20 +0100

    user/device/result route added

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,6 +3,17 @@ var redApp = angular.module('redApp', [
   'redControllers'
 ]);
 
+// redApp.config(['$httpProvider', function ($httpProvider) {
+//     $httpProvider.interceptors.push(function ($q, $cookies) {
+//         return {
+//             'request': function (config) {
+// 
+//                 config.headers['authorization'] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VybmFtZSI6Imp1bGlkb24iLCJwYXNzd29yZCI6InBhc3N3b3JkIiwiaWF0IjoxNDU2MjM0NzIyLCJleHAiOjE0NTYyMzUzMjJ9.uQpZoxna8MNK2foq3nuT9qyvcZ-VA1VXoIcGa9ooEW9cDSTvBIWk7vXmFUigDE4R5uNVIyOX7zLzlEMAMPp9N2Ff2KHINhNrCCLbveP2ur2soqo3i-8A83DMob65UB7NuAuk15TOeIjr3RZIeGl2cvUMvF6kOz8qdQesXSENTaLD67rppXnFdLAVwxpVGNse5HLNGORY85UHeSnnMwI11UBKB9uX_qqtXI9mitYUGi0wizUTUYRfs2wFGswOu6shexEgL1jPOGlw4mMMfiyPeMtx86wuLej5fuQnL9VT0UKu6cddaSN-LGf-SHoaNyj3uaeZci2czix1acSlcHxIXg";
+//                 return config;
+//             }
+//         };
+//     });   
+// }
 
 /* routes example
 redApp.config(['$routeProvider',
--- public/javascript/controllers.js
+++ public/javascript/controllers.js
@@ -7,7 +7,7 @@ redControllers.controller('authController', ['$scope', '$location', '$http', 'Au
     $scope.token = Auth.getToken();
 
     $scope.logIn = function () {
-        $http.post(url + '/user/login', $scope.userData)
+        $http.post(url + '/login', $scope.userData)
             .success(function (token) {
                 Auth.setToken(token);
                 Auth.setUser($scope.userData.username); //only for display
@@ -16,14 +16,15 @@ redControllers.controller('authController', ['$scope', '$location', '$http', 'Au
                 $scope.token = Auth.getToken();
                 console.log("ca marche");
             })
-            .error(function () {
+            .error(function (err) {
                 //TODO display error message
+                console.log(err);
                 console.log("ca marche pas");
             });
     }
 
     $scope.register = function () {
-        $http.post(url + '/user/register', $scope.userData)
+        $http.post(url + '/register', $scope.userData)
             .success(function (token) {
                 Auth.setToken(token);
                 Auth.setUser($scope.userData.username); //only for display
@@ -32,9 +33,23 @@ redControllers.controller('authController', ['$scope', '$location', '$http', 'Au
                 $scope.token = Auth.getToken();
                 console.log("ca marche");
             })
-            .error(function () {
+            .error(function (err) {
                 //TODO display error message
+                console.log(err);
                 console.log("ca marche pas");
             });
+    }
+    
+    $scope.all = function(){
+        $http.get(url + '/user/device/result',{    
+            headers: {
+                "authorization": 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VybmFtZSI6Imp1bGlkb24iLCJwYXNzd29yZCI6InBhc3N3b3JkIiwiaWF0IjoxNDU2MjM0NzIyLCJleHAiOjE0NTYyMzUzMjJ9.uQpZoxna8MNK2foq3nuT9qyvcZ-VA1VXoIcGa9ooEW9cDSTvBIWk7vXmFUigDE4R5uNVIyOX7zLzlEMAMPp9N2Ff2KHINhNrCCLbveP2ur2soqo3i-8A83DMob65UB7NuAuk15TOeIjr3RZIeGl2cvUMvF6kOz8qdQesXSENTaLD67rppXnFdLAVwxpVGNse5HLNGORY85UHeSnnMwI11UBKB9uX_qqtXI9mitYUGi0wizUTUYRfs2wFGswOu6shexEgL1jPOGlw4mMMfiyPeMtx86wuLej5fuQnL9VT0UKu6cddaSN-LGf-SHoaNyj3uaeZci2czix1acSlcHxIXg'
+            }})
+        .success(function(result){
+            $scope.result = result;
+        })
+        .error(function(err){
+            console.log(err);
+        });
     }
 }]);
